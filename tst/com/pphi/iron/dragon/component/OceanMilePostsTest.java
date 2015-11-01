94c68b71dc9662b4eb2da07ffd39c3359b55616f
draukadin <draukadin@gmail.com>
2015-11-01 14:52:48 -0800

    Updating the tests to be more meaningful

--- tst/com/pphi/iron/dragon/component/OceanMilePostsTest.java
+++ tst/com/pphi/iron/dragon/component/OceanMilePostsTest.java
@@ -3,14 +3,17 @@ package com.pphi.iron.dragon.component;
 import org.pphi.hexagon.coordinates.HexagonCubeCoordinate;
 import org.testng.annotations.Test;
 
-import java.util.Map;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
 
 public class OceanMilePostsTest {
 
     @Test
     public void testConstructOceanMilePosts() throws Exception {
         OceanMilePosts oceanMilePosts = new OceanMilePosts();
-        Map<HexagonCubeCoordinate, OceanMilePost> map = oceanMilePosts.getOceanMilePostMilePosts();
-        System.out.println(map.size());
+        assertNotNull(oceanMilePosts.getMilePost(new HexagonCubeCoordinate(-17 , 48, -31))); //First mile post in row
+        assertNotNull(oceanMilePosts.getMilePost(new HexagonCubeCoordinate(0 , 29, -29))); //MilePost in the middle of row definition
+        assertNotNull(oceanMilePosts.getMilePost(new HexagonCubeCoordinate(48 , -17, -31))); //Last mile post in row
+        assertNull(oceanMilePosts.getMilePost(new HexagonCubeCoordinate(0, 0, 0))); // Not a Ocean MilePost
     }
 }
--- tst/com/pphi/iron/dragon/component/PortMilePostsTest.java
+++ tst/com/pphi/iron/dragon/component/PortMilePostsTest.java
@@ -1,17 +1,22 @@
 package com.pphi.iron.dragon.component;
 
 
+import org.pphi.hexagon.coordinates.HexagonCubeCoordinate;
+import org.pphi.hexagon.util.CoordinateUtil;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
 
 public class PortMilePostsTest {
 
     @Test
     public void testPortsCreation() throws IOException {
         PortMilePosts portMilePosts = new PortMilePosts();
-        assertEquals(25, portMilePosts.getPorts().size());
+        assertNotNull(portMilePosts.getMilePost(new HexagonCubeCoordinate(-32, CoordinateUtil.solveForY(-32, 8), 8)));
+        assertNull(portMilePosts.getMilePost(new HexagonCubeCoordinate(0, 0, 0)));
     }
 }
