fc02fec37a98a996e298471efecbc516e8f9da64
draukadin <draukadin@gmail.com>
2015-11-05 08:29:17 -0800

    Created object to deserialize the mileposts into

---
+++ src/com/pphi/iron/dragon/board/MilePostJson.java
@@ -0,0 +1,23 @@
+package com.pphi.iron.dragon.board;
+
+import com.pphi.iron.dragon.component.Country;
+import edu.uci.ics.jung.graph.util.Pair;
+
+public class MilePostJson {
+
+    private Country country;
+    private Pair<Integer> pair;
+
+    public MilePostJson(Country country, Pair<Integer> pair) {
+        this.country = country;
+        this.pair = pair;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public Pair<Integer> getPair() {
+        return pair;
+    }
+}
