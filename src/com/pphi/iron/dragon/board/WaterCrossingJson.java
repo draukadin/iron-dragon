5b23ecbf06441d2283ada8cec0175537f996843b
draukadin <draukadin@gmail.com>
2015-11-15 20:40:43 -0800

    Removing country as an attribute since it is not needed on this object

--- src/com/pphi/iron/dragon/board/WaterCrossingJson.java
+++ src/com/pphi/iron/dragon/board/WaterCrossingJson.java
@@ -1,25 +1,17 @@
 package com.pphi.iron.dragon.board;
 
-import com.pphi.iron.dragon.component.Country;
-
 public class WaterCrossingJson {
 
     private String name;
-    private Country country;
     private WaterCrossingType waterCrossingType;
 
-    public WaterCrossingJson(String name, String country, WaterCrossingType waterCrossingType) {
+    public WaterCrossingJson(String name, WaterCrossingType waterCrossingType) {
         this.name = name;
-        this.country = Country.valueOf(country);
         this.waterCrossingType = waterCrossingType;
     }
 
     public String getName() {
         return name;
-    }
-
-    public Country getCountry() {
-        return country;
     }
 
     public WaterCrossingType getWaterCrossingType() {
