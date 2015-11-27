9bfb0dc79cc54797562b6d5d6cac703c218c47fb
draukadin <draukadin@gmail.com>
2015-11-26 17:33:22 -0800

    Adding the libgdx launcher files and gradle wrapper

---
+++ desktop/src/com/pphi/iron/dragon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pphi.iron.dragon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pphi.iron.dragon.IronDragon;
+
+public class DesktopLauncher {
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new IronDragon(), config);
+    }
+}
